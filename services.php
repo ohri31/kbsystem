4161b2d99f211e64ba3082ce81ba078c4574c233
Mirza Ohranovic <mirza.ohranovic@gmail.com>
2017-01-14 22:34:28 +0100

    k

--- services.php
+++ services.php
@@ -2,7 +2,15 @@
 	require_once('brains/global.php');
 	require_once('models/article.php');
 	/* Services doc */
-	if (isset($_GET)) {
+	if (isset($_POST)) {
+		/* Search */
+		if(isset($_POST['term'])){
+			$term = $_POST['term'];
+
+			$article = new Article;
+			$article->listArticles($term);
+		}
+	}else if (isset($_GET)) {
 		switch ($_GET['service']) {
 			case 'articles':
 				header("{$_SERVER['SERVER_PROTOCOL']} 200 OK");
@@ -19,14 +27,6 @@
 			default:
 				die("ERROR");
 				break;
-		}
-	}else if (isset($_POST)) {
-		/* Search */
-		if(isset($_POST['term'])){
-			$term = $_POST['term'];
-
-			$article = new Article;
-			$article->listArticles($term);
 		}
 	}else{
 		header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
